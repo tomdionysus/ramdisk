2afdd3572f081c6d7954927e00158a1e9c31c4fc
Tom Cully <tomhughcully@gmail.com>
2024-01-01 17:39:56 +1300

    Update README and LICENSE

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # ramdisk
 An experimental/teaching project to create a linux block driver kernel module for a ramdisk.
 
+This is a Linux 6.x.x+ kernel module that implements an in-memory blockstore, as a device `/dev/ramdisk`. The device is 100Mb in size.
+
+This is based on [Oleg Kutkov](https://olegkutkov.me/)'s work from 2020 - [Linux block device driver](https://olegkutkov.me/2020/02/10/linux-block-device-driver/), however there have been significant changes to the way the 6.x.x kernel handles queue allocation and registration, and his excellent example no longer compiles for this kernel version. 
+
+I'm hoping this might be useful as a starting point if you're writing your own block devices for this version of the linux kernel.  
+
 ## Build tools, linux headers, kernel module tools
 
 ```bash
@@ -25,4 +31,8 @@ make load
 ```bash
 make unload
 ```
+
+## License
+
+ramdisk is licensed under the GNU General Public License v3. Please see [LICENSE](LICENSE).
 
--- ramdisk.c
+++ ramdisk.c
@@ -1,12 +1,27 @@
 //
-// Ramdisk device driver
+// /dev/ramdisk device driver
+// 
+// Copyright (C) 2024 Tom Cully
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
 //
 // Original by Oleg Kutkov (https://olegkutkov.me/2020/02/10/linux-block-device-driver)
 // Adapted for linux-6.6.7-0-lts by Tom Cully 2023
 //
 // Tested under linux 6.6.7-0-lts
-// 
-
+//
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/slab.h>
